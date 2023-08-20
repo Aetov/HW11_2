6a1eb517e5b4bfb6684a018ba7636bbd375491ae
Aetov <142318947+Aetov@users.noreply.github.com>
2023-08-20 23:36:46 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def capital_letters():
     return str.upper(userinput)
 
 print(capital_letters())
+
+ """Эта функция переводит в заглавные буквы
+ 
+    слово, введенное пользователем с
+    помощью метода строки upper
+    """
