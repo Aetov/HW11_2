c3fa3d7aa833680c086a6d7b357fb6e146ad13af
Aetov Azamat <m3rc1kk@gmail.com>
2023-08-20 23:51:29 +0300

    Added first_capital_letters

--- str_func.py
+++ str_func.py
@@ -2,7 +2,7 @@ userinput = input()
 def capital_letters():
     """Это функция переводит всё слово
         в заглавные буквы, которое ввел
-        пользователь, при помощи метода str.upper.
+        пользователь, при помощи метода str.upper
     """
     return str.upper(userinput)
 
@@ -11,7 +11,7 @@ print(capital_letters())
 def first_capital_letters():
     """Это функция переводит только первую букву
         из слова, которое ввел
-        пользователь, при помощи метода str.capitalize.
+        пользователь, при помощи метода str.capitalize
     """
     return str.capitalize(userinput)
 
